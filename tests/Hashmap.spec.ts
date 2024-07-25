56aa6ab499e2c8c2fc614b343d4eb8a2fd5ef85a
CodeBag0923 <codemaven0923@gmail.com>
2024-07-25 13:31:37 -0300

    complete smart contract with using hashmaps and tests

--- tests/Hashmap.spec.ts
+++ tests/Hashmap.spec.ts
@@ -1,5 +1,5 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Cell, toNano } from '@ton/core';
+import { beginCell, Cell, toNano } from '@ton/core';
 import { Hashmap } from '../wrappers/Hashmap';
 import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
@@ -18,6 +18,8 @@ describe('Hashmap', () => {
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
+        blockchain.now = 500;
+
         hashmap = blockchain.openContract(Hashmap.createFromConfig({}, code));
 
         deployer = await blockchain.treasury('deployer');
@@ -28,12 +30,111 @@ describe('Hashmap', () => {
             from: deployer.address,
             to: hashmap.address,
             deploy: true,
-            success: true,
+            // success: true,
         });
+
+        await hashmap.sendSet(deployer.getSender(), toNano('0.05'), {
+            queryId: 123n,
+            key: 1n,
+            validUntil: 1000n,
+            value: beginCell().storeUint(123, 16).endCell().asSlice(),
+        })
+
+        await hashmap.sendSet(deployer.getSender(), toNano('0.05'), {
+            queryId: 123n,
+            key: 2n,
+            validUntil: 2000n,
+            value: beginCell().storeUint(123, 16).endCell().asSlice(),
+        })
+
+        await hashmap.sendSet(deployer.getSender(), toNano('0.05'), {
+            queryId: 123n,
+            key: 3n,
+            validUntil: 3000n,
+            value: beginCell().storeUint(123, 16).endCell().asSlice(),
+        })
+
+        
     });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and hashmap are ready to use
+    it('should store and retrieve values', async ()=> {
+        let [validUntil, value] = await hashmap.getByKey(deployer.getSender(), 1n);
+        expect(validUntil).toEqual(1000n);
+        expect(value).toEqualSlice(
+            beginCell().storeUint(123,16).endCell().asSlice()
+        );
+        
+        [validUntil, value] = await hashmap.getByKey(deployer.getSender(), 2n);
+        expect(validUntil).toEqual(2000n);
+        expect(value).toEqualSlice(
+            beginCell().storeUint(123,16).endCell().asSlice()
+        );
+        
+
+        [validUntil, value] = await hashmap.getByKey(deployer.getSender(), 3n);
+        expect(validUntil).toEqual(3000n);
+        expect(value).toEqualSlice(
+            beginCell().storeUint(123,16).endCell().asSlice()
+        );
+        
     });
+
+    it('should throw on not found key', async () => {
+        await expect(hashmap.getByKey(deployer.getSender() ,123n)).rejects.toThrow();
+    });
+
+    it('should clear old values', async ()=> {
+        await hashmap.sendClearOldValues(deployer.getSender(), toNano('0.05'), {
+            queryId:123n
+        });
+
+        let [validUntil, value] = await hashmap.getByKey(deployer.getSender(), 1n);
+        expect(validUntil).toEqual(1000n);
+        expect(value).toEqualSlice(
+            beginCell().storeUint(123, 16).endCell().asSlice()
+        );
+
+        blockchain.now = 1001;
+
+        await hashmap.sendClearOldValues(deployer.getSender(), toNano('0.05'), {
+            queryId: 123n
+        })
+
+        await expect(hashmap.getByKey(deployer.getSender(), 1n)).rejects.toThrow();
+
+
+        [validUntil, value] = await hashmap.getByKey(deployer.getSender(), 2n);
+        expect(validUntil).toEqual(2000n);
+        expect(value).toEqualSlice(
+            beginCell().storeUint(123, 16).endCell().asSlice()
+        );
+
+        blockchain.now = 2001;
+
+        await hashmap.sendClearOldValues(deployer.getSender(), toNano('0.05'), {
+            queryId: 123n
+        })
+
+        await expect(hashmap.getByKey(deployer.getSender(), 2n)).rejects.toThrow();
+
+        
+        [validUntil, value] = await hashmap.getByKey(deployer.getSender(), 3n);
+        expect(validUntil).toEqual(3000n);
+        expect(value).toEqualSlice(
+            beginCell().storeUint(123, 16).endCell().asSlice()
+        );
+
+        blockchain.now = 3001;
+
+        await hashmap.sendClearOldValues(deployer.getSender(), toNano('0.05'), {
+            queryId: 123n
+        })
+
+        await expect(hashmap.getByKey(deployer.getSender(), 3n)).rejects.toThrow();
+
+        
+    });
+
+
+
 });
--- wrappers/Hashmap.ts
+++ wrappers/Hashmap.ts
@@ -1,4 +1,4 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, Slice, TupleItemSlice } from '@ton/core';
 
 export type HashmapConfig = {};
 
@@ -25,5 +25,57 @@ export class Hashmap implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async sendSet(
+        provider: ContractProvider,
+        via: Sender,
+        value: bigint,
+        opts: {
+            queryId: bigint,
+            key: bigint,
+            value: Slice,
+            validUntil: bigint
+        }
+    ) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(1, 32)
+                .storeUint(opts.queryId, 64)
+                .storeUint(opts.key, 256)
+                .storeUint(opts.validUntil, 64)
+                .storeSlice(opts.value)
+                .endCell()
+        })
+    }
+
+    async sendClearOldValues(
+        provider: ContractProvider,
+        via: Sender,
+        value: bigint,
+        opts: {
+            queryId: bigint,
+        }
+    ) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(2, 32)
+                .storeUint(opts.queryId, 64)
+                .endCell()
+        })
+    }
+
+    async getByKey (
+        provider: ContractProvider,
+        via: Sender,
+        key: bigint,
+        
+    ): Promise<[bigint, Slice]> {
+        const result = (await provider.get('get_key',[{type: 'int', value: key}])).stack;
+        return [result.readBigNumber(), (result.peek() as TupleItemSlice).cell.asSlice()]
     }
 }
